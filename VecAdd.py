a7dda57c760a8fe743c715cc71d07409d50ff69b
TravisPace <40004711+TravisPace@users.noreply.github.com>
2018-06-24 19:58:19 -0500

    Update VecAdd.py

--- VecAdd.py
+++ VecAdd.py
@@ -5,13 +5,16 @@ def vecAddition(V1,V2):
   we return the list A.
   '''
   A = [ ]
+  #A is an empty list.
   if len(V1) != len(V2):
     print('error')
     return None
+  #If the lengths of the lists V1 and V2 do not equal each other, then this prints an error and returns none.
   for i in range(len(V1)):
     A.append(V1[i] + V2[i])
+    #This adds the lengths of V1 and V2 and adds each element of the list together.
   return A
-
+  #This returns the list A.
 
 V1 = [1,2]
 V2 = [3,4]
